f8eac19cb361039c9125281743d2242fba1c4de1
Petter Fagerlund <petter.fagerlund@gmail.com>
2018-01-27 16:07:21 -0500

    test

--- app/controllers/golfcourses_controller.rb
+++ app/controllers/golfcourses_controller.rb
@@ -1,2 +1,13 @@
 class GolfcoursesController < ApplicationController
+  def index
+    require "httparty"
+
+    url = "https://miami.craigslist.org/search/sof"
+
+    response = HTTParty.get url
+
+    puts response.body
+
+
+  end
 end
