191245ff97e45d1585e127de7a59ce52755083a0
Petter Fagerlund <petter.fagerlund@gmail.com>
2018-01-28 14:22:14 -0500

    Added /app/services/tee_time_api.rb add class TeeTimeApi and moved Api call here. Cleaned up controller to call that class method inste

--- app/controllers/golfcourses_controller.rb
+++ app/controllers/golfcourses_controller.rb
@@ -1,15 +1,12 @@
 class GolfcoursesController < ApplicationController
   def index
 
-  response = HTTParty.get('https://www.teeoff.com/TeeTime/ByCourse?contactId=&includeInventory=true&includeDirectoryRegular=true&includeDirectorySpecial=true&includeGSSpecial=true&includeSoldTeeTimes=false&includeForeDirectory=true&includeGSCourse=true&includeGSBarter=true&enforceTimeRestrictions=true&enforcePriceRestrictions=true&showFeaturedCourses=true&beginTime=5%3A00+AM&endTime=11%3A59+PM&beginTimeIndex=-1&minPrice=0&maxPrice=999&sort=2&viewType=1&page=1&start=0&limit=100&view=course&chosenAmenities=any&chosenCourses=any&playDate=01%2F28%2F2018&hasCartIncluded=false&centerPointLon=-80.2264393&centerPointLat=25.7889689&courseIds=XHZDBFQMgCEMX4gAoCczeQ0fqCg0e%2FA%2BNCejv86UzDbxuqkLoEcIt6nAxizZ0XlU3rCKOYRJ7xoVUjgJC27vePFB1OUKvoBRM1QKLtXEbyQZmWTAhmaQxL%2B281XZQI8AXsUjdZZ7HaxmYtvJKK155t7vH1CMe1WyO7zwPuulSWpZ09L75QjMGLJATFq1feJxlrAsbpTBFae7gKf4B&calculateDistanceFromUser=0&_=1517069125758')
-
-  body = response.body
-  body[0] = ''
-  body[-1] = ''
-  body[-1] = ''
-  @request = JSON.parse(body)
+    @request = TeeTimeApi.get_tee_times(params[:usersdate] || Date.today) # /services 
   end
 
-
+private
+  def search_params
+    params.require(:usersdate)
+  end
 
 end
