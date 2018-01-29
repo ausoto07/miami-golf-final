3806af0496bba938ff5fe8e05635e0f3604c6d29
Will Stone <Willstone109@aol.com>
2018-01-29 13:17:26 -0500

    update google maps api

---
+++ app/assets/config/keys.js
@@ -0,0 +1,6 @@
+
+(function() {if(process.Rake_env == "production") {
+
+} else {
+  return "https://maps.googleapis.com/maps/api/js?key=AIzaSyBNCexbhzCuZ7i0cSrjhzs64mYCYm4qKrU&callback=initMap";
+}}())
