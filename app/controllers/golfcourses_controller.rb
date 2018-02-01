1f4ae7a798d534afb28188cc6886adf46230ca7f
Petter Fagerlund <petter.fagerlund@gmail.com>
2018-01-31 19:00:17 -0500

    added stripe integration to modal, and .js file with algorythm to load on modal instead of main view. Added stripe to application_view.rb.

--- app/controllers/golfcourses_controller.rb
+++ app/controllers/golfcourses_controller.rb
@@ -5,6 +5,23 @@ class GolfcoursesController < ApplicationController
   end
 
   def send_booking_email
+    # Amount in cents
+    begin
+      @amount = @tee_time
+      customer = Stripe::Customer.create(
+        :email => params[:email],
+        :source  => params[:stripeToken]
+      )
+      charge = Stripe::Charge.create(
+        :customer    => customer.id,
+        :amount      => @amount,
+        :description => 'Rails Stripe customer',
+        :currency    => 'usd'
+      )
+    rescue Stripe::CardError => e
+      flash[:error] = e.message
+      # redirect_to root_path
+    end
 
     course_id = params['hidden_golfcourse_id'] #gets course_id from API
     @tee_time = params['teetime']
@@ -13,14 +30,15 @@ class GolfcoursesController < ApplicationController
     @telephone = params['telephone']
 
     email = TeeTimeApi::GOLFCOURSE_EMAILS[course_id] #gets the email of the corresponding course by id. See TeeTimeApi class.
-    TeetimeconfirmationMailer.send_booking_email(email, @tee_time, @name,@email, @telephone ).deliver_now
-    redirect_to root_path
+    TeetimeconfirmationMailer.send_booking_email(email, @tee_time, @name, @email, @telephone ).deliver_now
+    redirect_to booking_success_path
     return
   end
 
-private
-  def search_params
-    params.require(:usersdate)#users input in date_field_tag
-  end
+
+  private
+    def search_params
+      params.require(:usersdate)#users input in date_field_tag
+    end
 
 end
